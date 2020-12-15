73adaf0c3a1b5e203e67ed23c552993dccbca2c3
JoaoMateus1010 <joaomateus102030@gmail.com>
2020-12-15 08:53:41 -0300

    Ambietne so Software

--- src/software/main.c
+++ src/software/main.c
@@ -7,13 +7,24 @@
 
 #include <LPC11xx.h>
 
+#define DEBUG_FLAG 1
+
+#include "lib/adc.h"
+#include "lib/gpio.h"
+#include "lib/pwm.h"
+#include "lib/timer.h"
+
+#if DEBUG_FLAG
+#include "lib/uart.h"
+#endif
+
 
 int main(void) {      //Toggle a GPIO (FBD48 pin 23, PIO0_7, controls the LED on the LPC Expresso PCB)
  
      LPC_SYSCON->SYSAHBCLKCTRL   |= (1<<6);     //enable clock GPIO (sec 3.5.14)
      LPC_IOCON->PIO0_7           &= ~(0x10);    //NOT REQUIRED, turn off pull up (sec 7.4.19)
      LPC_GPIO0->DIR              |= (1<<7);     //set pin direction to output (sec 12.3.2)
- 
+     
      unsigned int i = 0;
  
      while(1){                      //infinite loop
