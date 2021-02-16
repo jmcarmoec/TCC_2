0bbe76a8e748ffca96f173a10036091d238457e6
JoaoMateus1010 <joaomateus102030@gmail.com>
2021-02-16 12:04:30 -0300

    Ponto de dúvida

--- src/software/lib/pwm.c
+++ src/software/lib/pwm.c
@@ -9,21 +9,21 @@ void init_PWM(){
       LPC_SYSCON->SYSAHBCLKDIV = 0x1; // Dividir o clock por 1 (pag. 29)
 
       /*
-            Olhar a página 341, para saber o que cada registrador abaixo faz.
+            Olhar a página 327, para saber o que cada registrador abaixo faz.
       */
 
-      LPC_TMR16B0->PR = CLOCK_SYS-1; // Define o prescale que é usado como granularidade do pwm, ou seja, 1 us de precisão no pulso (pag. 344)
-      LPC_TMR16B0->MR3 = PERIOD_SERVO_MOTOR_0*1000;//Define o período de 20 ms (pag. 345)
-      LPC_TMR16B0->MR0 = DEFAULT_DUTY_CYCLE_SERVO_MOTOR_0*1000; // Define o duty cycle de 1.7 ms (pag. 345)   
+      LPC_TMR16B0->PR = CLOCK_SYS-1; // Define o prescale que é usado como granularidade do pwm, ou seja, 1 us de precisão no pulso (pag. 329)
+      LPC_TMR16B0->MR3 = PERIOD_SERVO_MOTOR_0*1000;//Define o período de 20 ms (pag. 331)
+      LPC_TMR16B0->MR0 = PERIOD_SERVO_MOTOR_0*1000; // Define o duty cycle de 1.7 ms (pag. 331)   
       
-      LPC_TMR16B0->CTCR = 0x0; // Define o timer como contador, para cada borda de subida irá contar um valor. (pag. 349)
+      LPC_TMR16B0->CTCR = 0x0; // Define o timer como contador, para cada borda de subida irá contar um valor. (pag. 334)
 
-      LPC_TMR16B0->MCR |= 1<<10; // Reseta o Contador (Reg. TC) quando o contador chegar no valor definino como período (MR3) (pag. 345)
+      LPC_TMR16B0->MCR |= 1<<10; // Reseta o Contador (Reg. TC) quando o contador chegar no valor definino como período (MR3) (pag. 330)
 
-      LPC_TMR16B0->PWMC = 0x1; //Define PWM para o registrador MR0. (pag. 350)
+      LPC_TMR16B0->PWMC = 0x1; //Define PWM para o registrador MR0. (pag. 335)
 
-      LPC_TMR16B0->TCR = 0x2; //Reseta a contagem (pag. 343)
-      LPC_TMR16B0->TCR = 0x1; //Inicia contagem (pag. 343)
+      LPC_TMR16B0->TCR = 0x2; //Reseta a contagem (pag. 329)
+      LPC_TMR16B0->TCR = 0x1; //Inicia contagem (pag. 329)
 
 }
 
--- src/software/main.c
+++ src/software/main.c
@@ -17,25 +17,25 @@
 #include "lib/uart.h"
 #endif
 
-int main(void) { 
-      //init_GPIO();                        
+int main(void) {       
+      init_GPIO();                        
       init_UART();
       init_TIMER_0();
       init_PWM();
-      //set_DIR(3,OUTPUT);     
-      //char a = 0;          
+      set_DIR(3,OUTPUT);     
+      char a = 0;          
       while(1){
             
             printf_string("|");
             printf_int(LPC_TMR16B0->MR0);            
-            /*
+            
             if(a){
                  write_digital_gpio(3,HIGH);
                  a=0;
             }else{
                  write_digital_gpio(3,LOW);
                  a=1;
-            }*/
+            }
             delay_MS(1000);
       }
       return 0 ;
