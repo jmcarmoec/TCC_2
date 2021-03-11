5f862db8c98ac1351cc036279e57a3c1731011ef
JoaoMateus1010 <joaomateus102030@gmail.com>
2021-03-11 03:32:17 -0300

    Atualização do Hardware e função de interpolação pronta

--- src/software/system/system_sensors.c
+++ src/software/system/system_sensors.c
@@ -1,7 +1,7 @@
 #include "system_sensors.h"
 
 float get_lux_LDR(u_int8_t PIN_LDR){
-    float value=-1;
+    float value=-1;    
     switch(PIN_LDR){
         case (PIN_LDR_NORTH):
             select_ADC_channel(PIN_LDR_NORTH);
@@ -19,6 +19,9 @@ float get_lux_LDR(u_int8_t PIN_LDR){
             select_ADC_channel(PIN_LDR_EAST);
             value=V_AD(read_ADC(PIN_LDR_EAST));
         break;
-    }
-    return value;
+    }    
+    return interpolation_function(value);
+}
+float interpolation_function(float value){    
+    return 86.2264*expoente(value,7) - 1352.91*expoente(value,6) + 8889.32*expoente(value,5) - 31590.9*expoente(value,4) + 65337.4*expoente(value,3) - 78254.1*expoente(value,2) + 50073.6*value - 13081.7;
 }
--- src/software/system/system_sensors.h
+++ src/software/system/system_sensors.h
@@ -1,8 +1,12 @@
 #ifndef SYS_SENS
 #define SYS_SENS
-
+    
     #include "system_functions.h"
 
+    #define ERROR_MAX_LUX   (66.94231628)
+    #define ERROR_MEDIO_LUX (8,34816797)
+
     float get_lux_LDR(u_int8_t PIN_LDR);
+    float interpolation_function(float value);
 
 #endif
